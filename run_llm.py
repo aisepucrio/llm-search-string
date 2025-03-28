e674c03147bcf87c68342574e24da6bf112b4c33
DiogoMarassi <dmaraassi@gmail.com>
2025-03-28 11:20:37 -0300

    FLOPO-247 criando script para testes com llama e deepseek, registrando logs e tempo

--- run_llm.py
+++ run_llm.py
@@ -1,109 +1,62 @@
-import os
-import pathlib
 import json
-import signal
-import sys
 import ollama
 from tqdm.auto import tqdm
+from prompts import *
+import winsound
+import time
+from pdf_reader.pdf_reader_service import extract_data
+from ollama import chat
+from ollama import ChatResponse
 
-client = ollama.Client(host='http://localhost:11434')
+ollama_client = ollama.Client(timeout=60)
+print("iniciamos cliente")
 
-models = ['llama3:instruct', 'gemma:instruct', 'mistral:instruct', 'SentiStrength', 'SentiStrengthSE','SentiCR','DEVA','Senti4SD']
+models = ['deepseek-r1:14b']
 
-# Variável global para controlar a interrupção
-interrupted = False
+#inicioExtraction = time.time()
+#data = extract_data("pdf_reader/article")          
+#fimExtraction = time.time()
+#print(f"Tempo de execução llama: {fimExtraction - inicioExtraction:.2f} segundos")
 
-def signal_handler(sig, frame):
-    global interrupted
-    interrupted = True
-    print("Process interrupted. Saving progress...")
+#titulo = data[0][1]
+#resumo = data[0][2]
 
-def classify_pr(pr, model, prompt_content):
-    while True:
-        try:
-            response = ollama.generate(
-                model=model,
-                format="json",
-                options={
-                    "temperature": 1,
-                    "num_ctx": 8192,
-                    "num_predict": -1
-                },
-                stream=False,
-                prompt=f"""\
-Read the following message (in triple quotes, formatted as markdown):
+def salvar_em_txt(nome_arquivo, conteudo):
+    with open(nome_arquivo, 'a', encoding='utf-8') as arquivo:
+        arquivo.write(conteudo + '\n')  # adiciona uma quebra de linha no final
+    print(f'Conteúdo adicionado ao arquivo "{nome_arquivo}".')
 
-\"\"\"
-{pr}
-\"\"\"
+def run_llm(model, prompt_content, articleData):
+    print("Iniciando o llm")
+    print("prompt:\n")
+    print(f'''{prompt_content}\n{articleData}''')
+    print("\n")
+    response: ChatResponse = chat(model=model, messages=[
+    {
+        'role': 'user',
+        'content': f'''{prompt_content}\n{articleData}''',
+    },
+    ])
+    print(response['message']['content'])
+    print(response.message.content)
+    return response.message.content
 
-{prompt_content}.
-"""
-            )
-        except:
-            continue
-        break
-    try:
-        return json.loads(str(response['response']))['sentiment_polarity']
-    except:
-        print(response['response'])
-        print('Error in response')
 
-def classify_and_save(dataset, model, output_path, prompt, message_type, sample_size=None):
-    with open(dataset) as f:
-        dataset = json.load(f)
-    
-    # Carrega o arquivo existente ou inicializa um novo
-    if output_path.exists():
-        with open(output_path, "r") as f:
-            output_data = json.load(f)
-    else:
-        output_data = dataset
-
-    # Seleciona uma amostra se sample_size for especificado
-    if sample_size is not None:
-        dataset = dataset[:sample_size]
-    
-    # Adiciona a classificação ao dataset
-    for message in tqdm(dataset):
-        if interrupted:
-            break
-        llm_polarity = classify_pr(message[message_type], model, prompt)
-        if 'tools' not in message:
-            message['tools'] = {}
-        message['tools'][model] = llm_polarity
-    
-    # Atualiza output_data com as novas classificações
-    for idx, message in enumerate(output_data):
-        if idx < len(dataset):
-            message['tools'][model] = dataset[idx]['tools'][model]
-
-    # Salva o dataset atualizado no output_path
-    with open(output_path, "w") as f:
-        json.dump(output_data, f, indent=4)
-
-def get_prompt(prompt): 
-        with open(f"./prompts/{prompt}.txt") as f:
-            content = f.read()
-            if not content:
-                print("File is empjty")
-                sys.exit(1)
-            else:
-                return content
-
-def main(prompt_name, models, message_type, sample_size):
-    prompt = get_prompt(prompt_name)
-    dataset_path = pathlib.Path("./data/dataset.json")
-    output_path = pathlib.Path(fr"./data/analysis-{prompt_name}.json")
-
-    # Configura o manipulador de sinal para interrupções
-    signal.signal(signal.SIGINT, signal_handler)
-
+def main(models):
     for model in models:
-        if interrupted:
-            break
-        classify_and_save(dataset_path, model, output_path, prompt, message_type, sample_size)
+        for prompt, prompt_content in prompts.items():
+            for article in articles:
+                inicio = time.time()
+                answer = run_llm(model, prompt_content, article)
+                fim = time.time()
+                print(f"Tempo de execução llama: {fim - inicio:.2f} segundos")
+                save = prompt_content + '\n' + article + '\n' + "Answer:" + answer + '\n' + f"Tempo de execução llama: {fim - inicio:.2f} segundos" + "\n"
+                salvar_em_txt("log_deepseek14b.txt", save)
+                
+
     print("Classification completed.")
+    winsound.Beep(500, 5000)  
 
 if __name__ == '__main__':
-    main('prompt_base', models, 'clean_message', sample_size=None)
+    winsound.Beep(500, 1000)  
+    main(models)
